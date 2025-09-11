9ed193f85caf05e768b210d0023fc7553ec701f9
MI-NOTEBOOK-NIC\Nick <dolukianenko@gmail.com>
2025-09-11 13:05:29 +0200

    feat(web): add RecipesList component with RecipeCard stubs

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () => import('./intro/intro.component').then((m) => m.IntroComponent),
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   { path: '**', redirectTo: '' },
 ];
---
+++ apps/web/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IntroComponent } from "../intro/intro.component";
+import { RecipesListComponent } from "../recipes/recipes-list.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  imports: [IntroComponent, RecipesListComponent],
+})
+export class HomeComponent {}
